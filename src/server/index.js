c7dd3b02f71bb999a22c3ffced14de3074772f1f
okhatib <omar.i.elkhatib@gmail.com>
2021-04-09 15:56:48 +0200

    initial push
    - changed all css files to scss
    - added project dependencies
    - added the environment variable for the API_KEY
    - changed the prod listen port to 8081

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,11 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+// console.log(`Your API key is ${process.env.API_KEY}`);
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -9,13 +14,13 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
